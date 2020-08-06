af48a39f4ddcb7772bc88ca40461e1e3c5d4c2e1
Markus <amceehoo@gmail.com>
2020-08-06 03:52:44 +0300

    update source gitrepo

--- docker/run.sh
+++ docker/run.sh
@@ -26,12 +26,12 @@ printf '\n          Branch to build:  %s'  "${BRANCH_NAME}"
 printf '\n     -------------------------------------------------------------'
 printf '\n'
 
-docker build -f prod.dockerfile -t openvidu/openvidu-call:${RELEASE_VERSION} --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${CALL_BASE_HREF} .
-docker build -f prod.dockerfile -t openvidu/openvidu-call:${RELEASE_VERSION}-demos --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${DEMOS_BASE_HREF} .
+docker build -f prod.dockerfile -t mch03/openvidu-call:${RELEASE_VERSION} --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${CALL_BASE_HREF} .
+docker build -f prod.dockerfile -t mch03/openvidu-call:${RELEASE_VERSION}-demos --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${DEMOS_BASE_HREF} .
 
 printf '\n'
 printf '\n     Pushing containers to OpenVidu DockerHub'
 printf '\n'
 
-docker push openvidu/openvidu-call:${RELEASE_VERSION}
-docker push openvidu/openvidu-call:${RELEASE_VERSION}-demos
+docker push themch/openvidu-call:${RELEASE_VERSION}
+docker push themch/openvidu-call:${RELEASE_VERSION}-demos
