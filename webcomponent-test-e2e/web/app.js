d53c13438aae412e8a8179100f233fc346ae9120
Markus Hänninen <amceehoo@gmail.com>
2020-08-09 17:29:47 +0300

    korjausta

--- webcomponent-test-e2e/web/app.js
+++ webcomponent-test-e2e/web/app.js
@@ -103,7 +103,7 @@ async function joinSession(sessionName, user) {
  *   3) Configure OpenVidu Web Component in your client side with the token
  */
 
-var OPENVIDU_SERVER_URL = "https://hybrid.hyme.fi" ;
+var OPENVIDU_SERVER_URL = "https://localhost:4443" ;
 var OPENVIDU_SERVER_SECRET = 'jWSMFuVQC6D2d483S3Jd0';
 
 function getToken(sessionName) {
