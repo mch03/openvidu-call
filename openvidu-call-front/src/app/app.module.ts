c08ea0aa659e861df77da4b86f1aaa7d04a566ac
Markus Hänninen <amceehoo@gmail.com>
2020-08-24 17:46:38 +0300

    vieläkin korjataan

--- openvidu-call-front/src/app/app.module.ts
+++ openvidu-call-front/src/app/app.module.ts
@@ -24,8 +24,12 @@ import { createCustomElement } from '@angular/elements';
 import { ElementZoneStrategyFactory } from 'elements-zone-strategy';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ShareModule } from 'ngx-sharebuttons';
+import { ShareButtonsModule } from 'ngx-sharebuttons';
 import { ShareIconsModule } from 'ngx-sharebuttons/icons';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 // Pipes
 import { LinkifyPipe } from './shared/pipes/linkfy';
 import {
@@ -115,6 +119,7 @@ import { StorageService } from './shared/services/storage/storage.service';
 		HttpClientModule,
 		FlexLayoutModule,
 		ShareModule,
+    ShareButtonsModule,
     ShareIconsModule
 	],
 	entryComponents: [DialogErrorComponent, WebComponentComponent],
