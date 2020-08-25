36979c0709b84c06be1a8b067cfff0094cb74987
Markus Hänninen <amceehoo@gmail.com>
2020-08-25 14:59:26 +0300

    Siistimistä

--- openvidu-call-front/src/app/home/home.component.ts
+++ openvidu-call-front/src/app/home/home.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+// import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+
+import { uniqueNamesGenerator, NumberDictionary } from 'unique-names-generator';
+ 
+const KattoTeema = [
+  'Pelti',
+  'Tiili',
+  'Huopa',
+  'Lukkosauma',
+	'Konesauma',
+	'Katto',
+	'Kattoremontti',
+	'Tasakatto',
+	'Alumiinipeltikatto',
+	'Kattopinnoite'
+];
+
+const DesiTeema = [
+  'Desi',
+  'Delta',
+  'Puhdistusvaahto',
+  'Lukkosauma',
+	'Alfa',
+	'Delta',
+	'Automaatti',
+	'Epsilon',
+	'Omega',
+	'Lambda'
+];
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
 	public roomForm: FormControl;
 	public version = require('../../../package.json').version;
@@ -15,8 +44,9 @@ export class HomeComponent implements OnInit {
 	constructor(private router: Router, public formBuilder: FormBuilder) {}
 
 	ngOnInit() {
-		const randomName = uniqueNamesGenerator({ dictionaries: [adjectives, colors, animals], separator: '-', });
-		this.roomForm = new FormControl(randomName, [Validators.minLength(4), Validators.required]);
+    const numberDictionary = NumberDictionary.generate({ min: 100, max: 999 });
+		const randomName = uniqueNamesGenerator({ dictionaries: [colors, DesiTeema, numberDictionary], length: 3, separator: '-', style: 'capital' });
+		this.roomForm = new FormControl(randomName, [Validators.minLength(3), Validators.required]);
 	}
 
 	public goToVideoCall() {
